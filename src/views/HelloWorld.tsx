e3ab6c00d7d75835904f7bd71d0f363d82378e0e
卢毅 <eivmosn@163.com>
2023-01-25 07:42:29 +0800

    fix: change favicon stroke color

--- src/views/HelloWorld.tsx
+++ src/views/HelloWorld.tsx
@@ -15,7 +15,7 @@ export default defineComponent({
     return <main class='fc'>
       <div class='mt-14'>
         <n-switch v-model:value={this.active} size="large" v-slots={{
-          'checked-icon': () => <i class='i-lucide:sun text-12px text-yellow' />,
+          'checked-icon': () => <i class='i-lucide:sun text-12px' />,
           'unchecked-icon': () => <i class='i-lucide:moon text-12px' />,
         }}
           onUpdate:value={this.handleUpdateValue}
